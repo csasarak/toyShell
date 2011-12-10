d1ef1cfd6b63b81e5d28e8b3fb13387526a2cf2f
csasarak <chris.sasarak@gmail.com>
2011-12-10 17:15:59 -0500

    Can read in input

---
+++ cmd_exec.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "cmdexec.h"
+
+// This file will need more error handling code
+
+/* Execute the command string by first tokenizing it and then
+   forking the program
+*/   
+void exec_cmd(char *cmd){
+    
+}
---
+++ cmd_exec.h
@@ -0,0 +1,5 @@
+#ifndef CMD_EXEC
+#define CMD_EXEC
+
+void exec_cmd(char *cmd);
+#endif
