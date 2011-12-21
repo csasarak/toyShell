f36b87cdc3854cea5e99f3da9435a4a6bb236fee
cms5347 <cms5347@rit.edu>
2011-12-21 15:51:56 -0500

    Changed the history so that they can be iterated from oldest to most recent command

--- hist.c
+++ hist.c
@@ -6,60 +6,66 @@
 
 // Make thread safe by passing history structs to functions
 static char** history = NULL;
-static int first_cmd = HIST_SIZE - 1; // The most recent command
+static int oldest_cmd = 0; // The oldest command in history
 
 /* Add a new command string to the history, deleting
    one in the process if that would make more than HIST_SIZE
    commands in history. Return 0 on success, set errno and return -1 otherwise
 */
 int add_cmd(char *cmd){
-    //Initialize the memory if it isn't there
-    if(history == NULL)
-        history = (char**)calloc(HIST_SIZE, sizeof(char*));
+    static int last_cmd = 0; // The most recently added command
     
-    // Check if we need to wrap around
-    if(first_cmd == 0)
-        first_cmd = HIST_SIZE - 1;
-    else
-        first_cmd--;
+    if(history != NULL){
+        // Check if we need to wrap around
+        if(last_cmd == HIST_SIZE - 1)
+            last_cmd = 0;
+        else
+            last_cmd++;
 
-    if(history[first_cmd] != NULL){
-        // possible TODO: only reallocate if a larger memory chunk is needed
-        free(history[first_cmd]);
+        if(history[last_cmd] != NULL){
+            // possible TODO: only reallocate if a larger memory chunk is needed
+            free(history[last_cmd]);
+            oldest_cmd = (oldest_cmd < HIST_SIZE - 1) ? oldest_cmd + 1 : 0;
+        }
     }
-    history[first_cmd] = (char*)malloc(strlen(cmd));
+    else{ // Allocate memory for the history if it isn't there
+        history = (char**)calloc(HIST_SIZE, sizeof(char*));
+        oldest_cmd = last_cmd;
+    }
+    
+    history[last_cmd] = (char*)malloc(strlen(cmd));
 
     // Check for memory allocation errors
-    if(history[first_cmd] == NULL){
+    if(history[last_cmd] == NULL){
         return -1;
     }
     
-    strcpy(history[first_cmd], cmd);
+    strcpy(history[last_cmd], cmd);
     return 0;
 }
 
-/* This function can be used to iterate through the history.
-   It returns a pointer to the next string in the history, or
-   NULL if it has reached the end or there is no history.
-   Passing NULL in as the parameter will get the next value,
-   while anything else will cause iteration to restart from the beginning.
-   Not thread safe. 
+/* This function can be used to iterate through the history from the oldest to
+   the most recent command It returns a pointer to the next string in the
+   history, or NULL if it has reached the end or there is no history.  Passing
+   0 in as the parameter will get the next value, while anything else will
+   cause iteration to restart from the beginning and return the first history
+   item.
 */
-char* next_cmd(int reset){
-    static int curr_cmd = HIST_SIZE - 1;
 
+char* next_cmd(int reset){
+    static int curr_cmd = 0;
     // Reset the iteration if necessary
-    if(reset != (int)NULL){
-        curr_cmd = first_cmd;
-        return history[first_cmd];
+    if(reset){
+        curr_cmd = oldest_cmd;
+        return history[curr_cmd];
     }
 
     curr_cmd++;
 
-    if(curr_cmd > HIST_SIZE - 1){
+    if(curr_cmd == HIST_SIZE){
         curr_cmd = 0;
     }
-    else if(curr_cmd == first_cmd){
+    if(curr_cmd == oldest_cmd){
         return (char*) NULL;
     }
     return history[curr_cmd];
